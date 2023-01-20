5d3ab7aa7556cd0a4c1373add48f2460ae89d8f0
Parkjaeyong <pjjy92@gmail.com>
2023-01-20 22:54:51 +0900

    박재용 : 120803/120804/120805/120810 문제 풀기

---
+++ level_0/parkjaeyong/q120805/Main.java
@@ -0,0 +1,18 @@
+package level_0.parkjaeyong.q120805;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int num1 = 10;
+        int num2 = 5;
+        int ans = sol.solution(num1, num2);
+        System.out.println(ans);
+    }
+}
+
