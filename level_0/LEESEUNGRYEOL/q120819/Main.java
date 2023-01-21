8422abe7f4d3f89c6e120ef86b27fd1d8c52ec79
이승렬 <ibban9810@icloud.com>
2023-01-21 20:16:50 +0900

    Docs: README file 수정

---
+++ level_0/LEESEUNGRYEOL/q120819/Main.java
@@ -0,0 +1,51 @@
+package level_0.LEESEUNGRYEOL.q120822;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String my_string = new String("aaabbb");
+//      String my_string = "jaron";
+        String answer = sol.solution(my_string);
+        System.out.println(answer);
+    }
+}
+
+// 나의 풀이
+class Solution {
+    public String solution(String my_string) {
+        char[] a = new char[my_string.length()];
+        int cnt = 0;
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            a[cnt] = my_string.charAt(i);
+            cnt++;
+        }
+//        for (int i = 0 ;i < my_string.length(); i++) {
+//            System.out.println("a = " + a[i]);
+//        }
+//
+//        String answer = new String();
+//        answer = Arrays.toString(a);
+//        return answer;
+        String answer = new String("");
+        for (int i = 0 ;i < my_string.length(); i++) {
+            answer = answer + a[i];
+        }
+
+        return answer;
+    }
+}
+//
+//// 나의 코드를 간단화 시킨 것
+//class Solution {
+//    public String solution(String my_string) {
+//        String answer = "";
+//
+//        for(int i=my_string.length()-1; i>=0; i--){
+//            answer+=my_string.charAt(i);
+//        }
+//
+//        return answer;
+//    }
+//}
