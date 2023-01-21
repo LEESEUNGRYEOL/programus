2b4972930ad3f695efd5844429d0193e2741e14f
이승렬 <ibban9810@icloud.com>
2023-01-21 14:26:59 +0900

    Add: 2일차 문제 풀이 완료

--- level_0/LEESEUNGRYEOL/q120815/Main.java
+++ level_0/LEESEUNGRYEOL/q120815/Main.java
@@ -1,22 +1,21 @@
-package level_0.LEESEUNGRYEOL.q120814;
+package level_0.LEESEUNGRYEOL.q120815;
 
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int n = 7;
+        int n = 4;
         int answer = sol.solution(n);
         System.out.println(answer);
     }
 }
 
-class Solution{
+class Solution {
     public int solution(int n) {
-        if (n%7 == 0)
-        {
-            return n/7;
+        int i = 1;
+        while ((6 * i) % n != 0) {
+            i++;
         }
-        else {
-            return (n/7 + 1);
-        }
+        return i;
+
     }
 }
