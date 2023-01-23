50d7c171b672f495978623cc05c98c625ff54c86
kingaser <kignaser@naver.com>
2023-01-23 10:50:07 +0900

    Add: 4일차 문제 풀이 완료

---
+++ level_0/kanghyegwang/q120825/solution120825.java
@@ -0,0 +1,21 @@
+class solution120824 {
+    public String solution(String str, int n) {
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < n; j++) {       // n개만큼 반복문 돌리기
+                res += str.charAt(i);
+            }
+        }
+        return res;
+    }
+
+//    public String solution(String str, int n) {   repeat으로 반복문 돌리기
+//        String res = "";
+//        StringBuilder sb = new StringBuiler();
+//        for (char x : str.toCharArray()) {
+//            sb.append((x + "").repeat(n));
+//        }
+//        return sb.toString();
+//    }
+
+}
