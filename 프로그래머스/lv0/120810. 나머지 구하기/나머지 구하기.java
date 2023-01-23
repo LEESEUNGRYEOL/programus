96d4d1e3d7cb11c26756ef20eac22202b7063b7f
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-24 00:22:05 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int ans = sol.solution(20, 5);
+        System.out.println(ans);
+    }
+}
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100){
+        answer = num1 % num2;
+        }
+        return answer;
+    }
+}
