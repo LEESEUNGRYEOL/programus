d7e92789c091b5d743ebeadbc696587eb10ba349
Kuno1734 <dlsgus59@gmail.com>
2023-01-25 22:26:19 +0900

    Add: q120898 추가

--- level_0/LeeGeonHo/q120583/Main.java
+++ level_0/LeeGeonHo/q120583/Main.java
@@ -1,2 +1,22 @@
-package level_0.LeeGeonHo.q120583;public class Main {
+package level_0.LeeGeonHo.q120583;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] array	= {1, 1, 2, 3, 4, 5};
+        int n = 1;
+        System.out.println(sol.solution(array,n));
+    }
 }
+
+class Solution {
+        public int solution(int[] array, int n) {
+            int answer = 0;
+            for(int i=0; i<array.length; i++){
+                if(array[i]==n){
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
--- level_0/LeeGeonHo/q120813/Main.java
+++ level_0/LeeGeonHo/q120813/Main.java
@@ -1,2 +1,32 @@
-package level_0.LeeGeonHo.q120813;public class Main {
+package level_0.LeeGeonHo.q120813;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 10;
+        System.out.println(Arrays.toString(sol.solution(n)));
+    }
+}
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        int idx =0;
+
+        if(n%2==0){
+            answer = new int[n/2];
+        }else{
+            answer = new int[n/2+1];
+        }
+
+        for(int i=1; i<=n; i++){
+            if(i%2==1){
+                answer[idx]=i;
+                idx++;
+            }
+        }
+        return answer;
+    }
 }
