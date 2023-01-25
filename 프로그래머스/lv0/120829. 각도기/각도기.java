660160d5f14eb8579b94dd82301285ad4e315ed7
hunny-A <91798823+hunny-A@users.noreply.github.com>
2023-01-25 23:10:46 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,25 +1,22 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
-        boolean angleVal = angle > 0 && angle <= 180;
         boolean acute = angle>0 && angle < 90;
         boolean right = angle == 90;
         boolean obtuse = angle > 90 && angle < 180;
         boolean flat = angle == 180;
         
-        if(angleVal){
-            if(acute){
-                answer = 1;
-            }
-            else if(right){
-                answer = 2;
-            }
-            else if(obtuse){
-                answer = 3;
-            }
-            else if(flat){
-                answer = 4;
-            }
+        if(acute){
+            answer = 1;
+        }
+        else if(right){
+            answer = 2;
+        }
+        else if(obtuse){
+            answer = 3;
+        }
+        else if(flat){
+            answer = 4;
         }
         
         return answer;
