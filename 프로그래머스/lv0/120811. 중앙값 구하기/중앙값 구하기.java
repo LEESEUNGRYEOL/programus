f7aad63cc1169085b30a2bb6864eac59b0f99afe
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-26 23:24:22 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-class Solution {
-    public int solution(int[] array) {
-        Arrays.sort(array);
-        int answer = array[array.length/2];
-        return answer;
-    }
-}
