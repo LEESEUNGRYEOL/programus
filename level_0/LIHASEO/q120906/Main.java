3243e5821ddcbf6df98b7a8474035574fbbfbc86
LIHA <lihaoka27@gmail.com>
2023-01-27 00:01:41 +0900

    Fix: 7일차 서리하 문제풀이 수정 완료

--- level_0/LIHASEO/q120906/Main.java
+++ level_0/LIHASEO/q120906/Main.java
@@ -3,23 +3,22 @@ package level_0.LIHASEO.q120906;
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int ans = sol.solution(20);
+        int ans = sol.solution(52);
         System.out.println(ans);
     }
 }
 
 class Solution {
     public int solution(int n) {
-        int mod, modsum = 0;
-        n = 294;
+        int mod = 0;
         String str = Integer.toString(n);
         for (int i = 0; i < str.length(); i++) {
-            mod = n % (10^i);
-
-            modsum += mod;
-            System.out.println(modsum);
+            mod += str.charAt(i) - '0';
+//            int div = 10^i;
+//            if (n / div  <= 0) {
+//            }
         }
-        int answer = modsum;
+        int answer = mod;
         return answer;
     }
 }
