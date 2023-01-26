d148a0901603588ebba4d7eb4f67eed5b2b125e2
LIHA <lihaoka27@gmail.com>
2023-01-26 23:21:57 +0900

    Fix: 7일차 서리하 문제풀이 수정 완료

---
+++ level_0/LIHASEO/q120818/Main.java
@@ -0,0 +1,21 @@
+package level_0.LIHASEO.q120818;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int ans = sol.solution(62);
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if (n >= 0 && n <= 100 && n%7 == 0) {
+            answer = n/7;
+        } else if (n < 7 || n%7 != 0) {
+            answer = n/7 + 1;
+        }
+        return answer;
+    }
+
+}
