a1cbc127beac65227ce920b5e96293e59f03f635
이승렬 <ibban9810@icloud.com>
2023-01-26 13:23:52 +0900

    Add: 7일차 문제풀이 완료

--- level_0/LEESEUNGRYEOL/q120811/Main.java
+++ level_0/LEESEUNGRYEOL/q120811/Main.java
@@ -1,39 +1,20 @@
 package level_0.LEESEUNGRYEOL.q120811;
 
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int answer;
-        int n1 = 24;
-        int n2 = 100;
-        answer = sol.solution(n1);
-        System.out.println(answer);
-        answer = sol.solution(n2);
+        int[] array = {1,2,3,4,5};
+
+        answer = sol.solution(array);
         System.out.println(answer);
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int cnt = 0, i = 1, j = 0, flag = 0;
-        // 제곱수가 아닌경우
-        while (true) {
-            if (n % i == 0) {
-                if (i == j) {
-                    break;
-                }
-                j = n / i;
-            cnt++;
-            }
-            if (i == j) {
-                flag = 1;// 제곱수인 경우
-                break;
-
-            }
-            i++;
-        }
-        return flag == 0 ? 2 *cnt: 2*cnt-1;
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        return (array[array.length/2]);
     }
 }
