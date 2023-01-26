d90aabf5a633addae28b7f6ab2052c28a3c8c8f7
hunny-A <91798823+hunny-A@users.noreply.github.com>
2023-01-26 16:38:28 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,6 +1,12 @@
 class Solution {
     public int solution(int n) {
-        int ms = (int)Math.sqrt(n);
-        return ms*ms==n ?1:2;
+        int answer = 2;
+        
+        for(int i=1; i<n; i++){
+            if(i*i==n){
+                answer = 1;
+            }
+        }
+        return answer;
     }
 }
