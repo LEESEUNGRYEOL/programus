f35b125f960c0412f827bd4ec7de341897a34f6e
Kuno1734 <dlsgus59@gmail.com>
2023-01-27 15:48:05 +0900

    Add: 7일차 q120909 제출

---
+++ level_0/LeeGeonHo/q120909/Main.java
@@ -0,0 +1,21 @@
+package level_0.LeeGeonHo.q120909;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 144;
+        System.out.println(sol.solution(n));
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        Double D = Math.sqrt(n);
+        if(D-D.intValue()!=0){
+            answer = 2;
+        }else answer=1;
+
+        return answer;
+    }
+}
