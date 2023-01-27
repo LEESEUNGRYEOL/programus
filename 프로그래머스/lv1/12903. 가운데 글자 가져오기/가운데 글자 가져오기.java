f1ab98cf8e2117413be3f2d9718bd8b4ef84887e
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-27 16:04:48 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 12.46 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String s) {
+        String ans = (s.length() / 2 == 0) ? s[s.length()/2]s[s.length()/2 + 1] : s[s.length()/2] 
+        
+        return ans;
+    }
+}
