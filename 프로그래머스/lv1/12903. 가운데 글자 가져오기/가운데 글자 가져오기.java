06f320fff09ab1a55a0ec2a143701c5bfe9a5def
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-27 16:38:21 +0900

    Delete 프로그래머스/lv1 directory

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public String solution(String s) {
-        String ans = (s.length() / 2 == 0) ? s[s.length()/2]s[s.length()/2 + 1] : s[s.length()/2] 
-        
-        return ans;
-    }
-}
