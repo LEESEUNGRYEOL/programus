c746685d972086a406ac0ccd7af8e847c0cd1cf3
LIHA <122168823+liha-engineer@users.noreply.github.com>
2023-01-28 03:25:53 +0900

    Delete 프로그래머스/lv0 directory

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-      public String solution(String my_string) {
-
-        String answer;
-            answer = my_string.replaceAll("[aiueo]","");
-        return answer;
-    }
-}
