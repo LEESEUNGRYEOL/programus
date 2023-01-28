571edea524dd37bfe9bd2675a549bb3fba9fbd8b
Parkjaeyong <pjjy92@gmail.com>
2023-01-28 10:10:08 +0900

    fix : 9일차 박재용 120823문제 성능 개선

--- level_0/parkjaeyong/q120823/Main.java
+++ level_0/parkjaeyong/q120823/Main.java
@@ -1,16 +1,21 @@
 package level_0.parkjaeyong.q120823;
 
 
+import java.io.BufferedWriter;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
-class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        for(int i = 1 ; i <= n ; i++){
-            System.out.println("*".repeat(i));
-        }
+class Solution {
+    public static void main(String[] args)  {
+        try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            Scanner sc = new Scanner(System.in);
+            for(int i = 1, n = sc.nextInt() ; i <= n ; i++)
+                bw.write("*".repeat(i)+"\n");
+            bw.flush();
+        }catch (IOException e){}
     }
 }
 
